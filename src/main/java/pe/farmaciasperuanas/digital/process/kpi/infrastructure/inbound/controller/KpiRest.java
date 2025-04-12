f2a361699d779908db1722a85483bf92aab598a8
pieromarcoz <75951868+pieromarcoz@users.noreply.github.com>
2025-04-12 15:10:58 -0500

    fix

--- src/main/java/pe/farmaciasperuanas/digital/process/kpi/infrastructure/inbound/controller/KpiRest.java
+++ src/main/java/pe/farmaciasperuanas/digital/process/kpi/infrastructure/inbound/controller/KpiRest.java
@@ -9,9 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pe.farmaciasperuanas.digital.process.kpi.application.service.KpiServiceImpl;
 import pe.farmaciasperuanas.digital.process.kpi.domain.entity.Kpi;
+import pe.farmaciasperuanas.digital.process.kpi.domain.port.repository.KpiRepository;
+import pe.farmaciasperuanas.digital.process.kpi.infrastructure.outbound.adapter.KpiRepositoryImpl;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -38,9 +41,21 @@ public class KpiRest {
 
   @Autowired
   private KpiServiceImpl kpiService;
+    @Autowired
+    private KpiRepository kpiRepository;
 
   @GetMapping(value = {"/generate"})
   public Mono<Map<String, Object>> generateKpi() {
     return kpiService.generateKpiFromSalesforceData();
+  }
+
+  @GetMapping(value = {"/complete-kpis"})
+  public Mono<Map<String, Object>> completeKpis() {
+    log.info("Solicitud para completar KPIs para todas las campañas");
+    return ((KpiRepositoryImpl)kpiRepository).verifyAndCompleteKpis()
+            .then(Mono.just(new HashMap() {{
+              put("message", "Verificación y completado de KPIs finalizado");
+              put("status", "success");
+            }}));
   }
 }
